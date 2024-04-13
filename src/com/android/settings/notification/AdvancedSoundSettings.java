pa-gr/android_packages_apps_Settings

Settings: Re-organize sound settings
Co-authored-by: spkal01 <kalligeross@gmail.com>
Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Change-Id: I57a534c73ced53f0cb80b62499956c287eb424ad

src/com/android/settings/notification/AdvancedSoundSettings.java
@@ -1,0 +1,120 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.app.settings.SettingsEnums;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.widget.PreferenceCategoryController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SearchIndexable
+public class AdvancedSoundSettings extends DashboardFragment {
+
+    public static final String TAG = "AdvancedSoundSettings";
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.sound_settings_advanced;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, this, getSettingsLifecycle());
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
+            AdvancedSoundSettings fragment, Lifecycle lifecycle) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+
+        // === Other Sound Settings ===
+        final DialPadTonePreferenceController dialPadTonePreferenceController =
+                new DialPadTonePreferenceController(context, fragment, lifecycle);
+        final ScreenLockSoundPreferenceController screenLockSoundPreferenceController =
+                new ScreenLockSoundPreferenceController(context, fragment, lifecycle);
+        final ChargingSoundPreferenceController chargingSoundPreferenceController =
+                new ChargingSoundPreferenceController(context, fragment, lifecycle);
+        final DockingSoundPreferenceController dockingSoundPreferenceController =
+                new DockingSoundPreferenceController(context, fragment, lifecycle);
+        final TouchSoundPreferenceController touchSoundPreferenceController =
+                new TouchSoundPreferenceController(context, fragment, lifecycle);
+        final DockAudioMediaPreferenceController dockAudioMediaPreferenceController =
+                new DockAudioMediaPreferenceController(context, fragment, lifecycle);
+        final BootSoundPreferenceController bootSoundPreferenceController =
+                new BootSoundPreferenceController(context);
+        final EmergencyTonePreferenceController emergencyTonePreferenceController =
+                new EmergencyTonePreferenceController(context, fragment, lifecycle);
+        final ScreenshotSoundPreferenceController screenshotSoundPreferenceController =
+                new ScreenshotSoundPreferenceController(context, fragment, lifecycle);
+
+        controllers.add(dialPadTonePreferenceController);
+        controllers.add(screenLockSoundPreferenceController);
+        controllers.add(chargingSoundPreferenceController);
+        controllers.add(dockingSoundPreferenceController);
+        controllers.add(touchSoundPreferenceController);
+        controllers.add(dockAudioMediaPreferenceController);
+        controllers.add(bootSoundPreferenceController);
+        controllers.add(emergencyTonePreferenceController);
+        controllers.add(screenshotSoundPreferenceController);
+        controllers.add(new PreferenceCategoryController(context,
+                "other_sounds_and_vibrations_category").setChildren(
+                Arrays.asList(dialPadTonePreferenceController,
+                        screenLockSoundPreferenceController,
+                        chargingSoundPreferenceController,
+                        dockingSoundPreferenceController,
+                        touchSoundPreferenceController,
+                        dockAudioMediaPreferenceController,
+                        bootSoundPreferenceController,
+                        emergencyTonePreferenceController,
+                        screenshotSoundPreferenceController)));
+
+        return controllers;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.SOUND;
+    }
+
+    // === Indexing ===
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.sound_settings) {
+
+                @Override
+                public List<AbstractPreferenceController> createPreferenceControllers(
+                        Context context) {
+                    return buildPreferenceControllers(context, null /* fragment */,
+                            null /* lifecycle */);
+                }
+            };
+}

DragonEmperor9480/derpfest_packages_apps_Settings

src/com/android/settings/notification/AdvancedSoundSettings.java
@@ -1,0 +1,124 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.app.settings.SettingsEnums;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.widget.PreferenceCategoryController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SearchIndexable
+public class AdvancedSoundSettings extends DashboardFragment {
+
+    public static final String TAG = "AdvancedSoundSettings";
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.sound_settings_advanced;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, this, getSettingsLifecycle());
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
+            AdvancedSoundSettings fragment, Lifecycle lifecycle) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+
+        // === Other Sound Settings ===
+        final DialPadTonePreferenceController dialPadTonePreferenceController =
+                new DialPadTonePreferenceController(context, fragment, lifecycle);
+        final ScreenLockSoundPreferenceController screenLockSoundPreferenceController =
+                new ScreenLockSoundPreferenceController(context, fragment, lifecycle);
+        final ChargingSoundPreferenceController chargingSoundPreferenceController =
+                new ChargingSoundPreferenceController(context, fragment, lifecycle);
+        final DockingSoundPreferenceController dockingSoundPreferenceController =
+                new DockingSoundPreferenceController(context, fragment, lifecycle);
+        final TouchSoundPreferenceController touchSoundPreferenceController =
+                new TouchSoundPreferenceController(context, fragment, lifecycle);
+        final DockAudioMediaPreferenceController dockAudioMediaPreferenceController =
+                new DockAudioMediaPreferenceController(context, fragment, lifecycle);
+        final BootSoundPreferenceController bootSoundPreferenceController =
+                new BootSoundPreferenceController(context);
+        final EmergencyTonePreferenceController emergencyTonePreferenceController =
+                new EmergencyTonePreferenceController(context, fragment, lifecycle);
+        final VibrateIconPreferenceController vibrateIconPreferenceController =
+                new VibrateIconPreferenceController(context, fragment, lifecycle);
+        final ScreenshotSoundPreferenceController screenshotSoundPreferenceController =
+                new ScreenshotSoundPreferenceController(context, fragment, lifecycle);
+
+        controllers.add(dialPadTonePreferenceController);
+        controllers.add(screenLockSoundPreferenceController);
+        controllers.add(chargingSoundPreferenceController);
+        controllers.add(dockingSoundPreferenceController);
+        controllers.add(touchSoundPreferenceController);
+        controllers.add(vibrateIconPreferenceController);
+        controllers.add(dockAudioMediaPreferenceController);
+        controllers.add(bootSoundPreferenceController);
+        controllers.add(emergencyTonePreferenceController);
+        controllers.add(screenshotSoundPreferenceController);
+        controllers.add(new PreferenceCategoryController(context,
+                "other_sounds_and_vibrations_category").setChildren(
+                Arrays.asList(dialPadTonePreferenceController,
+                        screenLockSoundPreferenceController,
+                        chargingSoundPreferenceController,
+                        dockingSoundPreferenceController,
+                        touchSoundPreferenceController,
+                        vibrateIconPreferenceController,
+                        dockAudioMediaPreferenceController,
+                        bootSoundPreferenceController,
+                        emergencyTonePreferenceController,
+                        screenshotSoundPreferenceController)));
+
+        return controllers;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.SOUND;
+    }
+
+    // === Indexing ===
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.sound_settings_advanced) {
+
+                @Override
+                public List<AbstractPreferenceController> createPreferenceControllers(
+                        Context context) {
+                    return buildPreferenceControllers(context, null /* fragment */,
+                            null /* lifecycle */);
+                }
+            };
+}