CalyxOS/platform_packages_apps_Settings

Enable more featureful biometrics settings screen
* Enable AOSP's combined biometric settings screen for all devices.
  This new section offers toggles for whether biometrics can unlock
  the device or solely be used by apps. It takes the place of
  individual biometric unlock options and acts as a container for
  the face and fingerprint unlock options.

* Update preference and section title to be generic.

* Remove the "Ways to unlock" and "Use face or fingerprint to"
  categories and the intro text.

Co-authored-by: Oliver Scott <olivercscott@gmail.com>
Co-authored-by: Chirayu Desai <chirayu@calyxinstitute.org>
Issue: calyxos#1165
Change-Id: Ib188cabb762bd30036682f2e4248597b62e45cb0

src/com/android/settings/biometrics/activeunlock/ActiveUnlockStatusUtils.java
@@ -181,7 +181,8 @@ public class ActiveUnlockStatusUtils {
             return R.string.security_settings_fingerprint_preference_title;
         } else {
             // Default to original summary, but this case should never happen.
-            return R.string.security_settings_biometric_preference_title;
+            // ...except it will, since we allow combined biometrics settings without both!
+            return R.string.security_settings_biometric_preference_title_calyx;
         }
     }
 

src/com/android/settings/biometrics/combination/BiometricsSettingsBase.java
@@ -357,7 +357,8 @@ public abstract class BiometricsSettingsBase extends DashboardFragment {
         final ChooseLockSettingsHelper.Builder builder =
                 new ChooseLockSettingsHelper.Builder(getActivity(), this)
                         .setRequestCode(CONFIRM_REQUEST)
-                        .setTitle(getString(R.string.security_settings_biometric_preference_title))
+                        .setTitle(getString(
+                                R.string.security_settings_biometric_preference_title_calyx))
                         .setRequestGatekeeperPasswordHandle(true)
                         .setForegroundOnly(true)
                         .setReturnCredentials(true);

src/com/android/settings/biometrics/combination/CombinedBiometricStatusUtils.java
@@ -54,7 +54,7 @@ public class CombinedBiometricStatusUtils {
      * Returns whether the combined biometric settings entity should be shown.
      */
     public boolean isAvailable() {
-        return Utils.hasFingerprintHardware(mContext) && Utils.hasFaceHardware(mContext);
+        return Utils.hasFingerprintHardware(mContext) || Utils.hasFaceHardware(mContext);
     }
 
     /**
@@ -93,9 +93,11 @@ public class CombinedBiometricStatusUtils {
     public String getTitle() {
         UserManager userManager = mContext.getSystemService(UserManager.class);
         if (userManager != null && userManager.isProfile()) {
-            return mContext.getString(R.string.security_settings_work_biometric_preference_title);
+            return mContext.getString(
+                    R.string.security_settings_work_biometric_preference_title_calyx);
         } else {
-            return mContext.getString(R.string.security_settings_biometric_preference_title);
+            return mContext.getString(
+                    R.string.security_settings_biometric_preference_title_calyx);
         }
     }
 

DragonEmperor9480/derpfest_packages_apps_Settings

src/com/android/settings/biometrics/activeunlock/ActiveUnlockStatusUtils.java
@@ -181,7 +181,8 @@ public class ActiveUnlockStatusUtils {
             return R.string.security_settings_fingerprint_preference_title;
         } else {
             // Default to original summary, but this case should never happen.
-            return R.string.security_settings_biometric_preference_title;
+            // ...except it will, since we allow combined biometrics settings without both!
+            return R.string.security_settings_biometric_preference_title_custom;
         }
     }
 

src/com/android/settings/biometrics/combination/BiometricsSettingsBase.java
@@ -371,7 +371,8 @@ public abstract class BiometricsSettingsBase extends DashboardFragment {
         final ChooseLockSettingsHelper.Builder builder =
                 new ChooseLockSettingsHelper.Builder(getActivity(), this)
                         .setRequestCode(CONFIRM_REQUEST)
-                        .setTitle(getString(R.string.security_settings_biometric_preference_title))
+                        .setTitle(getString(
+                                R.string.security_settings_biometric_preference_title_custom))
                         .setRequestGatekeeperPasswordHandle(true)
                         .setForegroundOnly(true)
                         .setReturnCredentials(true);

src/com/android/settings/biometrics/combination/CombinedBiometricStatusUtils.java
@@ -54,7 +54,7 @@ public class CombinedBiometricStatusUtils {
      * Returns whether the combined biometric settings entity should be shown.
      */
     public boolean isAvailable() {
-        return Utils.hasFingerprintHardware(mContext) && Utils.hasFaceHardware(mContext);
+        return Utils.hasFingerprintHardware(mContext) || Utils.hasFaceHardware(mContext);
     }
 
     /**
@@ -93,9 +93,11 @@ public class CombinedBiometricStatusUtils {
     public String getTitle() {
         UserManager userManager = mContext.getSystemService(UserManager.class);
         if (userManager != null && userManager.isProfile()) {
-            return mContext.getString(R.string.security_settings_work_biometric_preference_title);
+            return mContext.getString(
+                    R.string.security_settings_work_biometric_preference_title_custom);
         } else {
-            return mContext.getString(R.string.security_settings_biometric_preference_title);
+            return mContext.getString(
+                    R.string.security_settings_biometric_preference_title_custom);
         }
     }
 